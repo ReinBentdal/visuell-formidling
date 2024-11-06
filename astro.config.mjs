494e001a43e32a9636f5f10b955374efd9cb4420
ReinBentdal <rein.bent@gmail.com>
2024-11-06 22:11:01 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+
+import lit from '@astrojs/lit';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), lit()]
+});
