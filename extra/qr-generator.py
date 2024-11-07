ced24789757fc8da9fb3752ad790f67a5a3654ba
ReinBentdal <rein.bent@gmail.com>
2024-11-07 11:16:09 +0100

    new qr codes

---
+++ extra/qr-generator.py
@@ -0,0 +1,17 @@
+import qrcode
+
+BASE = 'https://reinbentdal.github.io/visuell-formidling'
+
+links = {
+    'qr-home': '/',
+    'qr-done': '/done',
+    'qr-square': '/square',
+    'qr-cafe': '/cafe',
+    'qr-news-the-daily-tribune': '/the-daily-tribune',
+    'qr-news-elvfjord-bulletin': '/elvfjord-bulletin',
+    'qr-news-the-national-journal': '/the-national-journal',
+}
+
+for link in links:
+    img = qrcode.make(BASE + links[link])
+    img.save(f"{link}.png")
